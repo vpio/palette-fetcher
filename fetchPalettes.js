7d424a66aaf1389a799e06b514a5076d7e3e1d85
Pio Molina <vpio@comcast.net>
2019-04-25 16:42:52 -0400

    initial commit

---
+++ fetchPalettes.js
@@ -0,0 +1,19 @@
+document.getElementById('get-palette').addEventListener('submit', (e) => {
+  e.preventDefault();
+  let username = document.getElementById('username').value
+
+  fetch('http://localhost:3000/api/v1/palettes/' + username).then(data => {
+    return data.json()
+  }).then((data) => updatePage(data))
+  .catch(error => console.error(error));
+});
+
+function updatePage(colors){
+  console.log(colors);
+  colors = colors[colors.length-1].colors.split('#').filter(el => { return el != ""})
+  let listedColors = colors.map(color => {
+    return `<li class='list-group-item' style='background-color: #${color};'>` + '#' + color + '</li>'
+  })
+  console.log(colors);
+  document.getElementById('colors').innerHTML = listedColors.join('')
+}
